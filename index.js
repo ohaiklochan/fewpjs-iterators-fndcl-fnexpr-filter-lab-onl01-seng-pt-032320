8d82c9b59eaece943a080f85ba30297668dc7497
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 13:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const findMatching = (drivers, string) => {
   return drivers.filter(x => x.toLowerCase() === string.toLowerCase())
 }
@@ -8,4 +9,8 @@ const fuzzyMatch = (drivers, string) => {
 
 const matchName = (drivers, string) => {
   return drivers.filter(c => c.name === string)
+=======
+function findMatching(drivers, string) => {
+  return drivers.filter(x => x.toLowerCase() === string.toLowerCase())
+>>>>>>> 7fea74b970991e437137774ffc681dad95748137
 }
